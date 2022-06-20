e71708c95cf0b6a1d4840deda76d69d5d60d8ae6
marijnh01 <marijnh@hotmail.nl>
2022-06-20 11:55:00 +0200

    random enemies

---
+++ src/enemy.ts
@@ -0,0 +1,53 @@
+import * as PIXI from "pixi.js"
+import { Game } from "./game"
+
+
+export class Enemy extends PIXI.Sprite {
+
+    deadTexture : PIXI.Texture
+    game: Game
+    private alive : boolean = true
+    speedx: number
+    speedy: number
+    direction: number
+    
+    constructor(game: Game,texture: PIXI.Texture, deadTexture: PIXI.Texture) {
+        super(texture)
+        this.game = game
+        this.deadTexture = deadTexture
+        this.speedx = 2 + Math.random() * 1
+        this.speedy = 2 + Math.random() * 1
+        this.x = Math.random() * 2000
+        this.y = Math.random() * 2000
+        this.direction = Math.random()*1
+        this.on('pointerdown', () => this.killfish())
+        this.scale.set(0.5 +Math.random()*1)
+        const myfilter = new PIXI.filters.ColorMatrixFilter()
+        this.filters = [myfilter]
+        myfilter.hue(Math.random()*360, false)
+        if(this.direction<0.5){
+            this.direction = this.direction * 1
+        } else {
+            this.direction = this.direction * -1
+        }
+    }
+
+    update(delta:number) {
+        if (this.alive) {
+            this.x += this.speedx * this.direction
+            this.y += this.speedy * this.direction
+            if (this.x > 2000){ 
+                this.x = 0
+            } else if (this.x < 0){
+                this.x = 2000
+            }
+            if (this.y > 2000){ 
+                this.y = 0
+            } else if (this.y < 0){
+                this.y = 2000
+            }
+          }
+    }
+    
+
+}
--- src/game.ts
+++ src/game.ts
@@ -1,27 +1,79 @@
-import * as PIXI from 'pixi.js'
-import fishImage from "./images/fish.png"
-import bubbleImage from "./images/bubble.png"
-import waterImage from "./images/water.jpg"
+import * as PIXI from "pixi.js";
+import { Enemy } from "./enemy"
+import { Player } from "./player";
 
-//
-// STAP 1 - maak een pixi canvas
-//
-const pixi = new PIXI.Application({ width: 800, height: 450 })
-document.body.appendChild(pixi.view)
+import backgroundImage from "./images/background_nl.png";
+import locationImage from "./images/location.png";
+import selectedImage from "./images/selected.png";
+import enemyImage from "./images/spacepirate.png"
+import deadImage from "./images/bones.png"
+import playerImage from "./images/spacecraft.png";
 
-//
-// STAP 2 - preload alle afbeeldingen
-//
-const loader = new PIXI.Loader()
-loader.add('fishTexture', fishImage)
-      .add('bubbleTexture', bubbleImage)
-      .add('waterTexture', waterImage)
-loader.load(()=>loadCompleted())
+export class Game {
 
-//
-// STAP 3 - maak een sprite als de afbeeldingen zijn geladen
-//
-function loadCompleted() {
-    let fish = new PIXI.Sprite(loader.resources["fishTexture"].texture!)
-    pixi.stage.addChild(fish)
-}
+    pixi: PIXI.Application;
+    loader: PIXI.Loader;
+    background: PIXI.Sprite;
+    locations: Location[] = [];
+    enemies: Enemy[] = [];
+    player: Player;
+
+    constructor() {
+        this.pixi = new PIXI.Application({
+            width: screen.width,
+            height: screen.height
+        });
+        document.body.appendChild(this.pixi.view);
+        
+        this.loader = new PIXI.Loader();
+        this.loader
+            .add("playerTexture", playerImage)
+            .add("locationTexture", locationImage)
+            .add("selectedTexture", selectedImage)
+            .add("enemytexture", enemyImage)
+            .add("deadTexture", deadImage)
+            .add("backgroundTexture", backgroundImage);
+        document.body.appendChild(this.pixi.view)
+
+        this.loader.load(() => this.doneLoading())
+    }
+
+
+    doneLoading() {
+        //Background
+        this.background = new PIXI.Sprite(this.loader.resources["backgroundTexture"].texture!);
+        this.pixi.stage.addChild(this.background)
+
+        //enemy
+        for (let i = 0; i < 10; i++) {
+            console.log("spawned")
+            let enemy = new Enemy(this, this.loader.resources["enemytexture"].texture!, this.loader.resources["deadTexture"].texture!)
+            this.pixi.stage.addChild(enemy)
+            this.enemies.push(enemy)
+        }
+
+
+        //Player
+        this.player = new Player(this, this.loader.resources["playerTexture"].texture!);
+        this.pixi.stage.addChild(this.player)
+
+        this.pixi.stage.x = this.pixi.screen.width / 2
+        this.pixi.stage.y = this.pixi.screen.height / 2
+
+        this.pixi.ticker.add((delta) => this.update(delta))
+
+
+    }
+
+    update(delta: number) {
+        this.player.update(delta);
+
+      for (let enemy of this.enemies) {
+
+        enemy.update(delta)
+    }
+    }
+
+
+    }
+  
